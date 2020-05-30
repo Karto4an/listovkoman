c235d43d6425195d8efed390cfadb07f48759a27
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-05-30 20:23:08 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -14,4 +14,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('NzE2MzAxNjYzMTQwMzE1MTQ3.XtKVFA.Z7PEoSzHDYIbhNMrsx56KtJya_4')
+client.run('TOKEN')

a827b616a489df1399cc98376794368f5afc7c01
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-05-30 20:34:48 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -13,5 +13,8 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+        
+BOT1 = NzE2MzAxNjYzMTQwMzE1MTQ3
+BOT2 = .XtKXyQ.KXhvyWv3b_5sbBWzBYap3Uh9Kgw
 
-client.run(BOT_TOKEN)
+client.run(BOT1+BOT2)

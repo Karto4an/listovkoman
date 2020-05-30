16821a429eda0161770fe9475e6eff4b33262ecb
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-05-30 21:20:53 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -14,6 +14,10 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
         
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+        
 BOT1 = str("NzE2MzAxNjYzMTQwMzE1MTQ3")
 BOT2 = str(".XtKXyQ.KXhvyWv3b_5sbBWzBYap3Uh9Kgw")
 

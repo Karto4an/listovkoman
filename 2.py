cf10578fe3833c846e606a5b8e0d2f6bfb4c3a1a
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-06-01 21:26:07 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -17,6 +17,10 @@ async def on_message(message):
     if message.content.startswith('!enable'):
         idle = True
         await message.channel.send("Enabled:crown:")
+
+    if message.content.startswith('!disable'):
+        idle = False
+        await message.channel.send("Disabled:thumbsdown:")
      
 BOT1 = str("NzE2MzAxNjYzMTQwMzE1MTQ3")
 BOT2 = str(".XtKXyQ.KXhvyWv3b_5sbBWzBYap3Uh9Kgw")

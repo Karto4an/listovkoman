b31e1a9d046375d2eb7373e84394981d2315bed6
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-06-02 20:15:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import random
 
 from dotenv import load_dotenv
 
+count = 0
+
 client = discord.Client()  #initializing some stuff
 random.seed()
 
@@ -12,7 +14,9 @@ random.seed()
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 
-ads = (["Меня создал натурал @Karto4an#0420","88005553535 лучше позвонить чем у кого-то занимать","Каждому сосну за покупку участка в экологически чистой зоне","Мясные продукты отменного качества!Подтвержденное качество.","Жираф","Челиха","Шо ты сказааааав?","Беблеотечка"])
+ads = (["Меня создал натурал @Karto4an#0420","88005553535 лучше позвонить чем у кого-то занимать",
+        "Каждому сосну за покупку участка в экологически чистой зоне","Мясные продукты отменного качества! За слова отвечаем.",
+        "Жираф","Челиха","Шо ты сказааааав?","Беблеотечка"])
 
 @client.event
 async def on_ready():
@@ -22,7 +26,9 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    
+    count = int("count+1")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="на"count"розданых листовок"))
     await message.channel.send(random.choice(ads), tts = True)
 
     if message.content.startswith('$hello'):

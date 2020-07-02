f0d931c1cf371cfd309ddaa7c351df35368c50d8
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-07-02 15:42:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,25 @@ random.seed()
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 
-ads = (["Меня создал натурал @Karto4an#0420","88005553535 лучше позвонить чем у кого-то занимать",
+ads = (["Меня создал @Karto4an#0420","88005553535 лучше позвонить чем у кого-то занимать",
         "Каждому сосну за покупку участка в экологически чистой зоне","Мясные продукты отменного качества! За слова отвечаем.",
         "Жираф","Челиха","Шо ты сказааааав?","Беблеотечка","Активация Windows. Чтобы активировать Windows, перейдите в раздел Параметры."])
+
+sites = (["http://pixelsfighting.com/,
+         http://www.omfgdogs.com/,
+         http://chihuahuaspin.com/,
+         https://www.ascii-middle-finger.com/,
+         http://buzzybuzz.biz/,
+         https://thepigeon.org/,
+         http://yeahlemons.com/,
+         http://www.amialright.com/,
+         https://www.bouncingdvdlogo.com/,
+         http://notdayoftheweek.com/,
+         http://eelslap.com/,
+         http://corndog.io/,
+         https://heeeeeeeey.com/,
+         https://smashthewalls.com/,
+         https://thatsthefinger.com/"])
 
 @client.event
 async def on_ready():

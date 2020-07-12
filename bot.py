7083460d0402b070d8260a871fa0ff2b6b780d22
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-07-12 20:19:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,51 +1,17 @@
-import os
-
 import discord
-import random
-
+from discord.ext import commands
 from dotenv import load_dotenv
 
-count = 0
+client = discord.Client(command_prefix = ".")
 
-client = discord.Client()  #initializing some stuff
-random.seed()
-
-#getting token from heroku vars
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 
-ads = (["Меня создал @Karto4an#0420","88005553535 лучше позвонить чем у кого-то занимать",
-        "Каждому сосну за покупку участка в экологически чистой зоне","Мясные продукты отменного качества! За слова отвечаем.",
-        "Жираф","Челиха","Шо ты сказааааав?","Беблеотечка","Активация Windows. Чтобы активировать Windows, перейдите в раздел Параметры."])
-
-sites = (["http://pixelsfighting.com/",
-         "http://www.omfgdogs.com/",
-         "http://chihuahuaspin.com/",
-         "https://www.ascii-middle-finger.com/",
-         "http://buzzybuzz.biz/",
-         "https://thepigeon.org/",
-         "http://yeahlemons.com/",
-         "http://www.amialright.com/",
-         "https://www.bouncingdvdlogo.com/",
-         "http://notdayoftheweek.com/",
-         "http://eelslap.com/",
-         "http://corndog.io/",
-         "https://heeeeeeeey.com/",
-         "https://smashthewalls.com/",
-         "https://thatsthefinger.com/"])
-
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-	return
-
-if message.author == client.user:
-	if message.content.startswith('$web'):
-		await message.channel.send(random.choice(sites))
-	else:
-		await message.channel.send(random.choice(ads), tts = True)
-
+	print("ready")
+	
+@client.command
+async def ban(ctx, member: discord.Member, *, reason = None):
+	await member.ban(reason=reason)
 client.run(token)
